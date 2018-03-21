4cd812aa9c52e2d404c6f6c3adc7985938f2ff47
Ervis Zyka <erviszyka@gmail.com>
2018-03-21 23:06:37 +0200

    Adds support for Haskell

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,8 +1,59 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa-stable" . "http://melpa.org/packages/") t)
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote stack-ghci))
+ '(haskell-tags-on-save t)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (ghc-imported-from company-ghc haskell-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-hook 'haskell-mode-hook #'hindent-mode)
+
+(let ((my-stack-path (expand-file-name "~/.cabal/bin")))
+  (setenv "PATH" (concat my-stack-path path-separator (getenv "PATH")))
+  (add-to-list 'exec-path my-stack-path))
+
+(eval-after-load 'haskell-mode
+  '(progn
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+     (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+     (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+     (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+	(define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile)
+     (define-key haskell-mode-map [f8] 'haskell-navigate-imports)))
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-o") 'haskell-compile)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
+
+(add-to-list 'load-path "~/.cabal/share/ghc-mod-5.8.0.0")
+
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+
+(require 'company)
+(add-hook 'haskell-mode-hook 'company-mode)
+(add-to-list 'company-backends 'company-ghc)
 
 (defvar clojure-packages '(better-defaults
 			   projectile
